08d654052f3e56b6536c4b8e9cf6452b2dae203c
colin <wangyitian1129@gmail.com>
2026-01-30 17:28:22 +0800

    配置优化

--- deepseek/src/main/java/com/deepseek/controller/AiController.java
+++ deepseek/src/main/java/com/deepseek/controller/AiController.java
@@ -5,7 +5,6 @@ import com.deepseek.record.Person;
 import com.deepseek.tools.AiTool;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.tool.function.FunctionToolCallback;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +16,14 @@ import reactor.core.publisher.Flux;
 @RequestMapping(value = "/ai")
 public class AiController {
 
-    @Autowired
-    private ChatClient deepSeekChatClient;
+    private final ChatClient deepSeekChatClient;
 
     @Value("classpath:/prompts/weather.st")
     private Resource weatherResource;
+
+    public AiController(ChatClient deepSeekChatClient) {
+        this.deepSeekChatClient = deepSeekChatClient;
+    }
 
     @GetMapping(value = "/stream")
     public Flux<String> stream(String message) {
